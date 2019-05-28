df61ecc985e1c7ccfb6adbb0a1df524264730ae6
Redshade Robotics <42994411+redshaderobotics@users.noreply.github.com>
2019-05-28 20:30:59 +0530

    test extension file

---
+++ index.js
@@ -0,0 +1,37 @@
+const ArgumentType = require('../../extension-support/argument-type');
+const BlockType = require('../../extension-support/block-type');
+const Cast = require('../../util/cast');
+const log = require('../../util/log');
+
+class Scratch3NewBlocks {
+    constructor (runtime) {
+        this.runtime = runtime;
+    }
+
+    getInfo () {
+        return {
+            id: 'newblocks',
+            name: 'New Blocks',
+            blocks: [
+                {
+                    opcode: 'writeLog',
+                    blockType: BlockType.COMMAND,
+                    text: 'log [TEXT]',
+                    arguments: {
+                        TEXT: {
+                            type: ArgumentType.STRING,
+                            defaultValue: "hello"
+                        }
+                    }
+                }
+            ],
+            menus: {
+            }
+        };
+    }
+
+    writeLog (args) {
+        const text = Cast.toString(args.TEXT);
+        log.log(text);
+    }
+}
